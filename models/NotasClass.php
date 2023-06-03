467b77e4faa22695849a3e4a013dbc862b87c50d
arielAayala <ariel170802@gmail.com>
2023-06-03 20:49:24 -0300

    notas y anexosnotas

--- models/NotasClass.php
+++ models/NotasClass.php
@@ -25,23 +25,34 @@
 
         public static function getByIdEncargo($idEncargo){
             $con = new Connection();
-            $query = "SELECT * FROM notas WHERE idEncargo = $idEncargo";
+            $query = "SELECT n.idNota, n.idEncargo, 
+                n.idUsuarioCreador, p.nombrePsicopedagogo as nombreCreador, p.fotoPsicopedagogo as fotoCreador, 
+                n.idNuevoResponsable, g.nombrePsicopedagogo as nombreNuevoResponsable, g.fotoPsicopedagogo as fotoNuevoResponsable, 
+                n.fechaCreacionNota, n.comentarioNota, 
+                CONCAT('[', GROUP_CONCAT(CONCAT('{','\"idNotaAnexo\":', na.idNotaAnexo, 
+                ',\"nombreNotaAnexo\":','\"',na.nombreNotaAnexo,'\",', 
+                '\"urlNotaAnexo\":' , '\"', na.urlNotaAnexo,'\"','}') SEPARATOR ','), ']') AS notasAnexo 
+                FROM notas n 
+                INNER JOIN usuarios u on n.idUsuarioCreador = u.idUsuario 
+                INNER JOIN psicopedagogos p on u.idPsicopedagogo = p.idPsicopedagogo 
+                LEFT JOIN usuarios s on n.idNuevoResponsable = s.idUsuario 
+                LEFT JOIN psicopedagogos g on s.idPsicopedagogo = g.idPsicopedagogo 
+                LEFT JOIN notas_anexos na on na.idNota = n.idNota 
+                WHERE n.idEncargo = $idEncargo
+                GROUP BY n.idNota ";
             $datos=[];
             $resultado = $con -> query($query);
-            if ($resultado -> num_rows >=0 ){
+            if ($resultado -> num_rows >= 0 ){
                 while ($row = $resultado -> fetch_assoc()){
-                    $datos[]=[
-                        "idNota" => $row["idNota"],
-                        "idEncargo" => $row["idEncargo"],
-                        "idUsuarioCreador" => $row["idUsuarioCreador"],
-                        "idNuevoResponsable" => $row["idNuevoResponsable"],
-                        "fechaCreacionNota" => $row["fechaCreacionNota"],
-                        "comentarioNota" => $row["comentarioNota"]
-                    ];
+                    // Decodificar el campo "notasAnexo" de JSON a un objeto/array PHP
+                    $notasAnexo = $row["notasAnexo"]; // Obtener el campo "notasAnexo" como cadena JSON
+                    $row["notasAnexo"] = json_decode($notasAnexo);
+                    $datos[] = $row;
                 }
             }
             return $datos;
         }
+        
 
     }
 
